cddb90d74d336e287fdcb06863511a156827e413
Juyang Li <ucantseeit1000@gmail.com>
2023-07-27 16:12:27 +0800

    keep clean

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,2 +1,8 @@
-package tester;public class TestArrayDequeEC {
+package tester;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
 }
